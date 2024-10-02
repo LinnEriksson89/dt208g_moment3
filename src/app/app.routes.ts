14975f26da254b1b667ed36282a8be182d55ca55
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-10-02 17:38:53 +0200

    small scss-changes and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { StartComponent } from './start/start.component';
+import { ConvertComponent } from './convert/convert.component';
+import { TaskComponent } from './task/task.component';
 
 export const routes: Routes = [
-    { path: "404", component: NotFoundComponent },
-    { path: "**", component: NotFoundComponent }
+    { path: "index", title: "Start", component: StartComponent },
+    { path: "", redirectTo: "index", pathMatch: 'full' },
+    { path: "convert", title: "Konvertering", component: ConvertComponent },
+    { path: "task", title: "Om uppgiften", component: TaskComponent },
+    { path: "**", title: "404", component: NotFoundComponent }
 ];
