6eec375a07b4c619d17ba823f6681b2874d70186
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-10-02 17:38:29 +0200

    modules/shell for pages

---
+++ src/app/start/start.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  standalone: true,
+  imports: [],
+  templateUrl: './start.component.html',
+  styleUrl: './start.component.scss'
+})
+export class StartComponent {
+    catImage: string = "../images/cat.jpg";
+    catImageAlt: string = "Kattunge i skogen, bild från pixabay.com"
+}
